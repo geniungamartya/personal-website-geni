252cd0bd207eacb72689734471892cc69fc24d51
geniungamartya <geniungamartya@gmail.com>
2024-05-15 23:11:58 +0700

    chore: formatted code

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,22 @@
 export const metadata = {
-  title: 'Geniung Amartya',
-  description: 'Geniung Amartya\'s personal homepage',
-}
+  title: "Geniung Amartya",
+  description: "Geniung Amartya's personal homepage",
+};
 
 export default function Home() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold trac">
-        Hey, welcome!
-      </h1>
+      <h1 className="mb-8 text-2xl font-semibold trac">Hey, welcome!</h1>
       <p className="mb-4">
-        I am Geniung Amartya; a Data, IT, Photography & Videography enthusiast based in Indonesia. 
-        I currently work as Data Engineer and spending my spare time learning web development, take photography and videography and play sport.
+        I am Geniung Amartya; a Data, IT, Photography & Videography enthusiast
+        based in Indonesia. I currently work as Data Engineer and spending my
+        spare time learning web development, take photography and videography
+        and play sport.
       </p>
       <p className="mb-4">
-        Feel free to reach out for collaboration opportunities. Let's connect and create something remarkable together!
+        Feel free to reach out for collaboration opportunities. Let's connect
+        and create something remarkable together!
       </p>
     </section>
   );
-};
+}
