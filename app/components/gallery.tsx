6f85dd50669f809819968dba6b856d199abe0fa7
geniungamartya <geniungamartya@gmail.com>
2024-11-27 13:23:52 +0700

    chore: move photos constant

--- app/components/gallery.tsx
+++ app/components/gallery.tsx
@@ -11,7 +11,6 @@ import {
   useLightboxState,
 } from "yet-another-react-lightbox";
 import {
-  Photo,
   RenderImageContext,
   RenderImageProps,
   RowsPhotoAlbum,
@@ -19,33 +18,6 @@ import {
 import "react-photo-album/rows.css";
 
 const Lightbox = dynamic(() => import("@/app/components/lightbox"));
-
-function assetLink(asset: string, width: number) {
-  return `/_next/image?url=/images/${asset}&w=${width}&q=75`;
-}
-
-const breakpoints = [3840, 1920, 1080, 640, 384, 256, 128];
-
-const photos = [
-  { asset: "1.jpeg", width: 1200, height: 1600 },
-  { asset: "2.jpeg", width: 1200, height: 1600 },
-  { asset: "3.jpeg", width: 1200, height: 1600 },
-  { asset: "4.jpeg", width: 1200, height: 1600 },
-  { asset: "5.jpeg", width: 1200, height: 1600 },
-  { asset: "6.jpeg", width: 1200, height: 1600 },
-].map(
-  ({ asset, width, height }) =>
-    ({
-      src: assetLink(asset, width),
-      width,
-      height,
-      srcSet: breakpoints.map((breakpoint) => ({
-        src: assetLink(asset, breakpoint),
-        width: breakpoint,
-        height: Math.round((height / width) * breakpoint),
-      })),
-    }) as Photo,
-);
 
 function NextImageForPhotoAlbum(
   { alt = "", title, sizes }: RenderImageProps,
@@ -125,7 +97,7 @@ function NextImageForLightbox({ slide, offset, rect }: any) {
   );
 }
 
-export function Gallery(): JSX.Element {
+export function Gallery({ photos }: any): JSX.Element {
   const [index, setIndex] = React.useState(-1);
   return (
     <>
--- app/photo-video/page.tsx
+++ app/photo-video/page.tsx
@@ -1,10 +1,39 @@
 import React from "react";
 import { Gallery } from "@/app/components/gallery";
+import { Photo } from "react-photo-album";
+
+function assetLink(asset: string, width: number) {
+  return `/_next/image?url=/images/${asset}&w=${width}&q=75`;
+}
+
+const breakpoints = [3840, 1920, 1080, 640, 384, 256, 128];
+
+const photos = [
+  { asset: "1.jpeg", width: 1200, height: 1600 },
+  { asset: "2.jpeg", width: 1200, height: 1600 },
+  { asset: "3.jpeg", width: 1200, height: 1600 },
+  { asset: "4.jpeg", width: 1200, height: 1600 },
+  { asset: "5.jpeg", width: 1200, height: 1600 },
+  { asset: "6.jpeg", width: 1200, height: 1600 },
+].map(
+  ({ asset, width, height }) =>
+    ({
+      src: assetLink(asset, width),
+      width,
+      height,
+      srcSet: breakpoints.map((breakpoint) => ({
+        src: assetLink(asset, breakpoint),
+        width: breakpoint,
+        height: Math.round((height / width) * breakpoint),
+      })),
+    }) as Photo,
+);
 
 export default function Page() {
+  // const  photos = getPhotos();
   return (
     <>
-      <Gallery />
+      <Gallery photos={photos} />
     </>
   );
 }
