bc90da78dcbaf001458f28028057dbfe8e8ea7bc
geniungamartya <geniungamartya@gmail.com>
2024-11-27 13:46:53 +0700

    chore: add get photo metadata utils

--- app/photo-video/page.tsx
+++ app/photo-video/page.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 import { Gallery } from "@/app/components/gallery";
+
 import { Photo } from "react-photo-album";
+import sharp from "sharp";
+import path from "path";
 
 function assetLink(asset: string, width: number) {
   return `/_next/image?url=/images/${asset}&w=${width}&q=75`;
 }
 
 const breakpoints = [3840, 1920, 1080, 640, 384, 256, 128];
+
+let photos2: any = ["public/images/1.jpeg"];
+
+const getPhotoObjects = async (photos: any) => {
+  const photoObjects = await Promise.all(
+    photos.map(async (photo: any) => {
+      const imagePath = path.resolve(photo); // Adjust path as needed
+      const metadata = await sharp(imagePath).metadata();
+      return {
+        src: photo,
+        width: metadata.width,
+        height: metadata.height,
+      };
+    }),
+  );
+  return photoObjects;
+};
 
 const photos = [
   { asset: "1.jpeg", width: 1200, height: 1600 },
