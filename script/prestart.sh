e34a5f89ae97130bbae4d6510a815d392f8598e2
geniungamartya <geniungamartya@gmail.com>
2025-03-29 17:04:41 +0700

    feat: use prestart script to bootstraps stuff

--- script/prestart.sh
+++ script/prestart.sh
@@ -4,6 +4,8 @@ set -e
 set -x
 
 cd backend
+alembic upgrade head
+python -m script.initial_data
 python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../openapi.json
 cd ..
 mv openapi.json frontend/
